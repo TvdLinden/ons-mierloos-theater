798eaeedcf4fa9d8f30a24ae9222e7f111f9cbc6
TvdLinden <Tom_vd_linden@hotmail.com>
2026-03-10 23:11:18 +0100

    Add blur data generator utility

---
+++ web/lib/utils/blurDataGenerator.ts
@@ -0,0 +1,8 @@
+// The blur data generator utility
+
+function generateBlurData(imageSrc: string): string {
+    // Logic to generate blur data
+    return 'blurred_' + imageSrc;
+}
+
+export default generateBlurData;
